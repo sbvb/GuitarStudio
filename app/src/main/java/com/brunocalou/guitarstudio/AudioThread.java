619bc8c8c9c570d133b53202861d21ed5c31bc6c
brunocalou <brunocaloualves@gmail.com>
2015-12-23 14:28:49 -0200

    Changed code to work with the external microphone

--- app/src/main/java/com/brunocalou/guitarstudio/AudioThread.java
+++ app/src/main/java/com/brunocalou/guitarstudio/AudioThread.java
@@ -16,8 +16,9 @@ public class AudioThread extends Thread {
     short[] buffer;
     int sample_rate_in_hz = 44100;
     int input_channel_config = AudioFormat.CHANNEL_IN_MONO;
-    int buffer_size_in_short = 0;
+    int buffer_size = 0;
     int audio_format = AudioFormat.ENCODING_PCM_16BIT;
+    int audio_source = MediaRecorder.AudioSource.MIC;
     AudioRecord input_audio;
 
     //Output
@@ -29,23 +30,24 @@ public class AudioThread extends Thread {
     String LOG_TAG = "audio_thread";
 
     public AudioThread() {
-        buffer_size_in_short = AudioRecord.getMinBufferSize(sample_rate_in_hz, input_channel_config, audio_format);
-        buffer = new short[buffer_size_in_short];
+        buffer_size = AudioRecord.getMinBufferSize(sample_rate_in_hz, input_channel_config, audio_format);
+        buffer = new short[buffer_size];
+        this.setPriority(Thread.MAX_PRIORITY);
     }
 
     @Override
     public void run() {
         super.run();
-        input_audio = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, sample_rate_in_hz, input_channel_config, audio_format, buffer_size_in_short);
-        output_audio = new AudioTrack(stream_type, sample_rate_in_hz, output_channel_config, audio_format, buffer_size_in_short, AudioTrack.MODE_STREAM);
+        input_audio = new AudioRecord(audio_source, sample_rate_in_hz, input_channel_config, audio_format, buffer_size);
+        output_audio = new AudioTrack(stream_type, sample_rate_in_hz, output_channel_config, audio_format, buffer_size, AudioTrack.MODE_STREAM);
 
         input_audio.startRecording();
         output_audio.play();
         try {
             while (!Thread.currentThread().isInterrupted()) {
                 while (is_playing) {
-                    input_audio.read(buffer, 0, buffer_size_in_short);
-                    output_audio.write(buffer, 0, buffer_size_in_short);
+                    input_audio.read(buffer, 0, buffer_size);
+                    output_audio.write(buffer, 0, buffer_size);
                 }
             }
         } catch (IllegalArgumentException e) {
